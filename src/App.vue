35c8caadb4862ac45a1cba2e8321bb1b80e9cfd1
Danel Filippov <Danel.Filippov@tptlive.ee>
2026-02-19 09:37:15 +0200

    28.01 tund

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,50 @@
 <script setup>
-    import { ref } from 'vue';
-    import { ref, computed } from 'vue';
+import { ref, computed } from 'vue';
+import { ref } from 'vue';
+import Modal from './components/Modal.vue';
 
-    let items = ref(['Sai', 'Piim', 'Viin', 'Ma', 'Tahan', 'Magada'])
-    let i = 1;
-    let itemss = ref(
-        [
-            {id: i++, text: 'Piim', is_done: false},
-            {id: i++, text: 'Sai', is_done: true},
-            {id: i++, text: 'Viin', is_done: true},
-            {id: i++, text: 'Ma', is_done: false},
-            {id: i++, text: 'Tahan', is_done: false},
-            {id: i++, text: 'Magada',is_done: false},
-          ]);
-    let new_item = ref('')
+let i = 1;
+let items = ref(
+    [
+      {id: i++, text: 'Piim', is_done: false},
+      {id: i++, text: 'Sai', is_done: true},
+      {id: i++, text: 'Viin', is_done: true},
+      {id: i++, text: 'Ma', is_done: false},
+      {id: i++, text: 'Tahan', is_done: false},
+      {id: i++, text: 'Magada',is_done: false},
+    ]);
+let new_item = ref('')
+let modal1Active = ref(false);
 
-    function add_shi()
-    {
-      if (new_item.value.trim())
-      {
-        items.value.push(new_item.value);
-        new_item.value = ''
-        items.value.push( {id: i++, text: new_item.value, is_done: false});
-        new_item.value = '';
-      }
-    }
+function add_shi()
+{
+  if (new_item.value.trim())
+  {
+    items.value.push( {id: i++, text: new_item.value, is_done: false});
+    new_item.value = '';
+  }
+}
 
-    let done_items = computed(() => {return items.value.filter(i => i.is_done)});
-  </script>
+let done_items = computed(() => {return items.value.filter(i => i.is_done)});
+</script>
 
-  <template>
-    @@ -21,7 +32,7 @@
-    <input class="input" type="text" v-model="new_item" @keypress.enter="add_shi">
-    <div class="control">
-      <button class="button is-info" @click="add_shi"></button>
+<template>
+  <div class="container">
+    <div class="field has-addons mt-3">
+      <div class="control is-expanded">
+        <input class="input" type="text" v-model="new_item" @keypress.enter="add_shi">
+      </div>
+      <div class="control">
         <button class="button is-primary" @click="add_shi">
           Add shit
         </button>
+      </div>
     </div>
-    @@ -30,11 +41,24 @@
+    <button class="button is-primary" @click="modal1Active=true">Open Modal</button>
+
     <div class="content">
       <h1>All items</h1>
       <ul>
-        <li v-for="item in items">{{ item }}</li>
         <li v-for="item in items">
           {{ item.text }}
           <input type="checkbox" v-model="item.is_done">
@@ -59,4 +61,11 @@
         </li>
       </ul>
     </div>
+    <Modal :active="modal1Active" @close="modal1Active=false"></Modal>
+  </div>
 </template>
+
+<style>
+
+</style>
+<style></style>
