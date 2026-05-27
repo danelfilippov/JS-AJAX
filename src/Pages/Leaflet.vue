672f21e488d026994ef9fa00e0516b5459b15c21
Danel Filippov <Danel.Filippov@tptlive.ee>
2026-05-27 13:42:37 +0300

    Leaflet map

---
+++ src/Pages/Leaflet.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import LeafletMap from '../components/LeafletMap.vue'
+
+let center = ref([59.42695235928513, 24.74360317137484])
+let zoom = ref(18)
+let marker = ref([59.42695235928513, 24.74360317137484])
+
+</script>
+
+<template>
+  <button class="is-primary button" @click="
+        center = [-10.246906785278114, -54.99352489937641];
+        marker = [-10.246906785278114, -54.99352489937641]">
+    Go to Brazil
+  </button>
+  <LeafletMap :center="center" :zoom="zoom" :marker="marker"></LeafletMap>
+</template>
